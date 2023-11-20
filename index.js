872d796a969ef1f7a11aab03b13bbe6c37428ca9
Dungha125 <dungha122405@gmail.com>
2023-11-20 19:55:00 +0700

    dungha

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const button = document.querySelector('.btn_first');
 const nightback = document.querySelector('.night');
 const zodiaclogo = document.querySelector('.zodiac');
 const images = document.querySelector('.carousel');
+const returnbtn = document.querySelector('.backbtn');
 button.addEventListener('click', function(){
     nightback.style.display = 'none';
     zodiaclogo.style.display = 'none';
     button.style.display = 'none';
     images.style.display = 'block';
 })
+returnbtn.addEventListener('click',function()
+{
+    nightback.style.display = 'flex';
+    zodiaclogo.style.display = 'block';
+    button.style.display = 'block';
+    images.style.display = 'none';
+})
+
 
 
